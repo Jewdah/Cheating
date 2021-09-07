7aaa9180a9c9707949b577b610e2fea7f96fb0d7
David Gamba <dgambami@uninpahu.edu.co>
2021-09-06 21:24:10 -0500

    Subida develop1

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express()
 app.get("/", function (req, res) {
  res.send("Primer GET")
 })
+app.get("/2", function (req, res) {
+    res.send("Segundo GET")
+   })
+app.get("/3", function (req, res) {
+    res.send("Tercer Get")
+   })
+app.get("/4", function (req, res) {
+    res.send("Cuarto Get ")
+   })
+   app.get("/5", function (req, res) {
+    res.send("Cinco Get ")
+   })
+   app.get("/6", function (req, res) {
+    res.send("Seis Get ")
+   })
+
 
  
 app.listen(3000)
